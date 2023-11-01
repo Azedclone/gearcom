e26ab3101580c6ce0c2a847b6165d15149a359b5
TLong <thanhlongvu156@gmail.com>
2023-11-01 10:13:05 +0700

    fix: duplicate data

--- src/main/java/com/prm392/gearcom/service/BillService.java
+++ src/main/java/com/prm392/gearcom/service/BillService.java
@@ -1,24 +1,19 @@
 package com.prm392.gearcom.service;
 
-import com.prm392.gearcom.api.model.BillBody;
 import com.prm392.gearcom.model.Bill;
-import com.prm392.gearcom.model.BillDetail;
 import com.prm392.gearcom.model.User;
 import com.prm392.gearcom.repository.BillRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class BillService {
     private final BillRepository billRepository;
 
-    public BillService(BillRepository _billRepository
-                       ) {
+    public BillService(BillRepository _billRepository) {
         this.billRepository = _billRepository;
-//        this.billDetailService = _billDetailService;
     }
 
     public Bill createBill(@AuthenticationPrincipal User user) {
